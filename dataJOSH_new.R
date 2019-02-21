5aeb3bb28c0c531c44c63407e41a9d4e44f704e7
Chaitanya Acharya <c.acharya@duke.edu>
2019-02-21 16:38:33 -0500

    updated with documentation

--- dataJOSH_new.R
+++ dataJOSH_new.R
@@ -1,13 +1,42 @@
 ## CCA
 
-library(Seurat); library(plyr)
-source("~/Research/scripts/r_scripts/useful_functions.R")
-source("~/Research/scripts/r_scripts/plotfns.R")
+## Set your working directory
+
+setwd() ## setwd("your directory")
+
+## Install BiocManager if not yet installed
+if(!requireNamespace("BiocManager")) install.packages("BiocManager")
+
+## Load the required R libraries
+## If the libraries are not installed, use BiocManager
+lib.list=c("Seurat","plyr","ggpubr","dplyr","ggthemes","data.table","GSVA")
+
+for(i in 1:length(lib.list)){
+	if(any(installed.packages()[,1]==lib.list[i])){
+		library(lib.list[i],character.only=T)}else{
+			BiocManager::install(lib.list[i])
+			library(lib.list[i],character.only=T)
+    };
+}
+
+## R scripts with utility functions
+# source("~/Research/scripts/r_scripts/useful_functions.R")
+# source("~/Research/scripts/r_scripts/plotfns.R")
+# Assuming that these R scripts are in the current working dir.
+source("useful_functions.R")
+source("plotfns.R")
+
+### Read the gene-set file
 ach = read.gmt.file("~/Research/pathways/mouse_pathways/acharya_genesets_mouse.gmt")
+
+### Read all the possible .tsv files and identity select files
+### Create a list object containing gene expression matrices of the select files
 files = list.files(pattern=".tsv")
 files = files[files %in% c("MT1.tsv","TU1.tsv","TU2.tsv","TU3.tsv","TU4.tsv")]
 data_list = llply(1:length(files),.progress="time",function(i) read.delim(files[i],header=T,row.names=1))
 
+### Create a SEURAT object from each gene expression matrix within the list
+### Each SEURAT object is preprocessed separately and highly variable genes are computed
 seu_list = gene_list = vector(mode="list",length=length(data_list))
 for(i in 1:length(seu_list)){
   print(i)
@@ -22,10 +51,10 @@ for(i in 1:length(seu_list)){
   gene_list[[i]] = genes_use
 }
 
-genes.use <- names(which(table(genes.use) > 1))
-for (i in 1:length(ob.list)) {
-  genes.use <- genes.use[genes.use %in% rownames(ob.list[[i]]@scale.data)]
-}
+### Run multivariate Canonical Correlation Analysis (CCA) on the common genes across all the genesets
+### Calculate the ratio of total variance explained by PPCA vs total variance explained by CCA, and filter cells based on these values
+### CCA Align the matrices and compute the alignment metric score
+### Map a t-SNE plot and find clusters
 
 pdf("CCA_plots.pdf",width=10,height=7)
 cca_out = RunMultiCCA(seu_list,genes.use=Reduce("intersect",gene_list),num.ccs = 20)
@@ -39,8 +68,6 @@ cca_out = AlignSubspace(cca_out, reduction.type = "cca", grouping.var = "group",
 CalcAlignmentMetric(cca_out,reduction.use = "cca.aligned",dims.use = 1:10, grouping.var =  "group")
 cca_out = RunTSNE(cca_out, reduction.use = "cca.aligned", dims.use = 1:10, do.fast = T)
 TSNEPlot(object = cca_out, do.label = F,group.by="group")
-# cca_out_umap = RunUMAP(cca_out,reduction.use="cca.aligned",seed=1234,n_neighbors = 30L,min_dist = 0.05)
-# DimPlot(cca_out_umap,reduction.use = 'umap')
 cca_out = FindClusters(cca_out, reduction.type = "cca.aligned", resolution = c(0.6), dims.use = 1:10,print.output = F)
 TSNEPlot(object = cca_out, do.label =T,no.legend = FALSE,dark.theme = FALSE,label.size=5)
 FeaturePlot(cca_out, features.plot = c("Krt5","Krt14","Krt6a"), reduction.use="tsne",nCol=2,min.cutoff = "q05", max.cutoff = "q95", cols.use = c("lightgrey", "red"), pt.size = 1)
@@ -49,31 +76,38 @@ FeaturePlot(cca_out, features.plot = c("Il6","Il12b","Serpinb2","Cxcl3"), reduct
 FeaturePlot(cca_out, features.plot = c("Arg1","Mrc1","Egr2","Cd83"), reduction.use="tsne",nCol=2,min.cutoff = "q05", max.cutoff = "q95", cols.use = c("lightgrey", "red"), pt.size = 1)
 FeaturePlot(cca_out, features.plot = c("Ptprc","Foxp3","Cd3d","Cd4","Cd14"), reduction.use="tsne",nCol=2,min.cutoff = "q05", max.cutoff = "q95", cols.use = c("lightgrey", "red"), pt.size = 1)
 
-library(GSVA); library(ggpubr); library(dplyr); library(ggthemes); library(data.table);
+### Compute sample-set GSEA (ssGSEA) scores
+## Plor
+library(GSVA); library(ggpubr); library(dplyr) library(ggthemes); library(data.table);
 ssgsea_out = gsva(as.matrix(cca_out@data),gset.idx.list=ach$genesets,method="ssgsea",kcdf="Gaussian",min.sz=1,max.sz=1000)
+pam50 = scan("~/Research/pathways/pam50_genes.txt",what="")
+pam50_mm = convertHumanGeneList(pam50)
+pam50_ssgsea = gsva(as.matrix(cca_out@data),gset.idx.list=list(pam50_mm),method="ssgsea",kcdf="Gaussian",min.sz=1,max.sz=1000)
+ssgsea_out = rbind(ssgsea_out,pam50_ssgsea)
+rownames(ssgsea_out)[49] = "PAM50"
 FM = ssgsea_out
 FM = as.matrix(Matrix::t(scale(Matrix::t(FM))))
 FM = melt(data.frame(t(FM),"Cluster"=cca_out@ident,"Group"=cca_out@meta.data$group))
 colnames(FM)[3:4]=c("GeneSets","EnrichmentScore")
+
+### Heatmap of cluster-wise gene-set enrichment scores for every gene-set
 ES_mean = FM %>% dplyr::group_by(Cluster,GeneSets,Group) %>% summarize_all(funs(mean))
 ggplot(ES_mean, aes(Cluster, GeneSets)) + geom_tile(aes(fill = EnrichmentScore), colour = "white") + scale_fill_gradient2(low ="dark blue", high ="dark red", mid ="white",space = "Lab") +
       theme_minimal() + theme(axis.text.x=element_text(size=8,angle=90)) + facet_grid(~Group)
+
+### Heatmap of cluster-specific gene-set enrichment scores
 ES1_mean = FM[,-1] %>% dplyr::group_by(GeneSets,Group) %>% summarize_all(funs(mean))
 ggplot(ES1_mean, aes(Group, GeneSets)) + geom_tile(aes(fill = EnrichmentScore), colour = "white") + scale_fill_gradient2(low ="dark blue", high ="dark red", mid ="white",space = "Lab") +
       theme_minimal() + theme(axis.text.x=element_text(size=8,angle=90))
 
+### Gene-set feature plots
 cca_out@meta.data = cbind(cca_out@meta.data,t(as.matrix(Matrix::t(scale(Matrix::t(ssgsea_out))))))
 FeaturePlot(cca_out,features.plot=c("BASAL","LUMINAL","MASC","STROMAL","LUMINAL PROGENITORS","EMT"),reduction.use="tsne",nCol=3,min.cutoff = "q05", max.cutoff = "q95", cols.use = c("lightgrey", "red"), pt.size = 1)
 FeaturePlot(cca_out,features.plot=c("MACROPHAGE_ACTIVITY","Treg","INFLAMMATORY RESPONSE","NK_CELLS","TCELL_ACTIVATION","G2M CHECKPOINT"),reduction.use="tsne",nCol=3,min.cutoff = "q05", max.cutoff = "q95", cols.use = c("lightgrey", "red"), pt.size = 1)
 FeaturePlot(cca_out,features.plot=c("HYPOXIA","INVASIVENESS GENE SIGNATURE","ANGIOGENESIS","ECM","FIBROBLAST GROWTH FACTOR RECEPTOR","EPIGENETIC STEM CELL"),reduction.use="tsne",nCol=3,min.cutoff = "q05", max.cutoff = "q95", cols.use = c("lightgrey", "red"), pt.size = 1)
 FeaturePlot(cca_out,features.plot=c("WNT BETA CATENIN SIGNALING","TGF BETA SIGNALING","E2F TARGETS","P53 PATHWAY","HEDGEHOG SIGNALING","PI3K-AKT-MTOR SIGNALING"),reduction.use="tsne",nCol=3,min.cutoff = "q05", max.cutoff = "q95", cols.use = c("lightgrey", "red"), pt.size = 1)
 FeaturePlot(cca_out,features.plot=c("BASE EXCISION REPAIR","MISMATCH EXCISION REPAIR","NUCLEOTIDE EXCISION REPAIR","HOMOLOGOUS RECOMBINATION","NONHOMOLOGOUS END JOINING"),reduction.use="tsne",nCol=3,min.cutoff = "q05", max.cutoff = "q95", cols.use = c("lightgrey", "red"), pt.size = 1)
-
-pam50 = scan("~/Research/pathways/pam50_genes.txt",what="")
-pam50_mm = convertHumanGeneList(pam50)
-pam50_ssgsea = gsva(as.matrix(cca_out@data),gset.idx.list=list(pam50_mm),method="ssgsea",kcdf="Gaussian",min.sz=1,max.sz=1000)
-cca_out@meta.data$PAM50 = as.numeric(scale(apply(pam50_ssgsea,1,scale)))
 FeaturePlot(cca_out,features.plot=c("PAM50"),reduction.use="tsne",nCol=1,min.cutoff = "q05", max.cutoff = "q95", cols.use = c("lightgrey", "red"), pt.size = 1)
-
+###
 
 dev.off()
